68f35a065a66145deb08acd3ef83b4960aab747c
None <120342992+SioNone@users.noreply.github.com>
2023-09-22 19:54:28 +0100

    Player Controllers and Greyboxing
    Added Player Controllers and Greyboxing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 0 = Neutral, 1 = Strike, 2 = Throw, 3 = Block
+    public int playerState = 0;
+
+    // Controls
+    public string strikeButton, throwButton, blockButton;
+
+    [Header("Action Properties")]
+    // Time until actions become active
+    public float strikeStartup, throwStartup, blockStartup;
+
+    // How long actions are active for
+    public float strikeActive, throwActive, blockActive;
+
+    // Time for actions to recover
+    public float strikeRecovery, throwRecovery, blockRecovery;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Strike
+        if (Input.GetKeyDown(strikeButton))
+        {
+            playerState = 1;
+        }
+
+        // Throw
+        if (Input.GetKeyDown(throwButton))
+        {
+            playerState = 2;
+        }
+
+        // Block
+        if (Input.GetKeyDown(blockButton))
+        {
+            playerState = 3;
+        }
+    }
+}
