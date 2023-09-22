6ab01627b8c1ba2e7105c60ced4cca7a6de1e1b8
None <120342992+SioNone@users.noreply.github.com>
2023-09-22 23:51:14 +0100

    Updated Scripts
    Added Player Interactions, working on moving players after interactions next

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -4,9 +4,70 @@ using UnityEngine;
 
 public class GameManagement : MonoBehaviour
 {
-    // Update is called once per frame
+    public GameObject playerOne, playerTwo;
+    public int playerOneScore, playerTwoScore;
+    public int playerOneState, playerTwoState;
+
+    void Start()
+    {
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+
     void Update()
     {
-        
+        Player playerOneScript = playerOne.GetComponent<Player>();
+        Player playerTwoScript = playerTwo.GetComponent<Player>();
+
+        playerOneState = playerOneScript.playerState;
+        playerTwoState = playerTwoScript.playerState;
+
+        // There's probably a better way of doing this part below (maybe? Idk)
+
+        // If a player exits neutral
+        if (playerOneState != 0 || playerTwoState != 0)
+        {
+            // Checks all instances where player 1 can begin and win an interaction
+            if ((playerOneState == 1 && (playerTwoState != 3 || playerTwoState == 0)) || (playerOneState == 2 && (playerTwoState == 3 || playerTwoState == 0)))
+            {
+                Debug.Log("Player 1 Pushes");
+
+                // Move Players | Players Move Right
+
+                // Update Scores
+                playerOneScore += 1;
+                playerTwoScore -= 1;
+            }
+
+            // Checks all instances where player 2 can begin and win an interaction
+            if ((playerTwoState == 1 && (playerOneState != 3 || playerOneState == 0)) || (playerTwoState == 2 && (playerOneState == 3 || playerOneState == 0)))
+            {
+                Debug.Log("Player 2 Pushes");
+
+                // Move Players | Players Move Left
+
+                // Update Scores
+                playerTwoScore += 1;
+                playerOneScore -= 1;
+            }
+
+            // Checks all instances where a clash occurs
+            if (playerOneState == playerTwoState)
+            {
+                Debug.Log("Clash");
+            }
+        }
+
+        if (playerOneScore == 5 || playerTwoScore == 5)
+        {
+            if (playerOneScore > playerTwoScore)
+            {
+                Debug.Log("Player 1 Win");
+            } 
+            else
+            {
+                Debug.Log("Player 2 Win");
+            }
+        }
     }
 }
